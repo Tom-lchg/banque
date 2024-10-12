091344bbfef5f30ef305e85a3e409ee6319c742c
Tom-Lchg <tommusique974@gmail.com>
2024-10-12 10:22:38 +0200

    initial commit

---
+++ vue/connexion.php
@@ -0,0 +1,15 @@
+<section id="connexion">
+    <form method="POST" action="connexion.php">
+    <div class="form-group">
+    <label for="email">Email</label>
+    <input type="email" class="form-control" id="email" name="email" placeholder="Email"
+    required>
+    </div>
+    <div class="form-group">
+    <label for="mdp">Mot de passe</label>
+    <input type="password" class="form-control" id="mdp" name="mdp" placeholder="Mot de
+    passe" required>
+    </div>
+    <button type="submit" class="btn btn-primary">Se connecter</button>
+    </form>
+</section>
---
+++ vue/inscription.php
@@ -0,0 +1,28 @@
+<section id="inscription">
+    <form method="POST" action="ajouter_client.php">
+    <div class="form-group">
+    <label for="nom">Nom</label>
+    <input type="text" class="form-control" id="nom" name="nom" placeholder="Nom" required>
+    </div>
+    <div class="form-group">
+    <label for="prenom">Prénom</label>
+    <input type="text" class="form-control" id="prenom" name="prenom" placeholder="Prénom"
+    required>
+    </div>
+    <div class="form-group">
+    <label for="telephone">Téléphone</label>
+    <input type="tel" class="form-control" id="telephone" name="telephone"
+    placeholder="Téléphone">
+    </div>
+    <div class="form-group">
+    <label for="email">Email</label>
+    <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+    </div>
+    <div class="form-group">
+    <label for="mdp">Mot de passe</label>
+    <input type="password" class="form-control" id="mdp" name="mdp" placeholder="Mot de
+    passe" required>
+    </div>
+    <button type="submit" class="btn btn-primary">S&apos;inscrire</button>
+    </form>
+</section>
